d0872d0490196277f7c861a53e7c93a5527a0b7d
[andreiVasilkevich] <[andrey.vasilkevich@bk.ru]>
2022-10-11 20:51:23 +0300

    folf

---
+++ src/components/element/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Search from '../../../UI/Search/Search'
+
+const Header = () => {
+  return (
+    <div>
+        <a href = '/'>
+            <img src='' alt='' />
+        </a>
+        <Search/>
+    </div>
+  )
+}
+
+export default Header
